78b2f6e0bff838f5fd4a67ec7a1f1ba45d05735a
jn2112 <93005114+jn2112@users.noreply.github.com>
2025-08-06 18:52:47 -0500

    hashed password and create account update

--- server/connect.py
+++ server/connect.py
@@ -3,6 +3,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
 from client.views import views_bp
+
+
+
 connect_bp = Blueprint("connect_bp", __name__)
 
 
@@ -17,20 +20,25 @@ def checkUser():
       
       name = request.form["username"]
       password = request.form["password"]
+  
       result = sqlite_test(name, password)
       
       if result: 
-
-        session['name'] = result['first_name'] + ' ' + result['last_name']
-        print(session['name'])
-        session['role'] = result['role']
-        print(session['role'])
-        
-        return jsonify({
-            "message": "Success",
-        }), 200
-          
-
+        if(check_password_hash(result['password'], password)):
+            session['name'] = result['first_name'] + ' ' + result['last_name']
+            print(session['name'])
+            session['role'] = result['role']
+            print(session['role'])
+            
+            return jsonify({
+                "message": "Success",
+            }), 200
+        else:
+          return jsonify({
+              "message": "Password is Incorrect. Please try again.",
+          }), 200
+  
+         
       else:
           return jsonify({
               "message": "Username/Password is Incorrect. Please try again.",
@@ -45,7 +53,7 @@ def sqlite_test(formName, formPassword):
     c = conn.cursor()
    
     
-    query = "SELECT * FROM ids WHERE username='"+formName+"' and password='"+formPassword+"'"
+    query = "SELECT * FROM ids WHERE username='"+formName+"'"
     c.execute(query)
     results = c.fetchone()
     
@@ -65,3 +73,44 @@ def sqlite_test(formName, formPassword):
 #        return False
 
 
+@connect_bp.route("/createUser", methods=["POST"])
+def createUser():    
+    if request.method =='POST':
+      username = request.form["username"]
+      password = request.form["password"]
+      firstName = request.form["firstname"]
+      lastName = request.form["lastname"]
+      role = request.form["role"]
+      print(username, password, firstName, lastName, role)
+      results = sqlite_addUser(username, password, firstName, lastName, role)
+      if results: 
+      
+        return jsonify({
+                  "message": "Success",
+              }), 200
+      
+    else:
+      return jsonify({
+          "message": "Data error. Please try again.",
+      }), 200
+  
+
+
+def sqlite_addUser(username, password, firstName, lastName, role): 
+    conn = sqlite3.connect(db_locale)
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    
+    hashed_password = generate_password_hash(password)
+    
+    query = "INSERT INTO ids(username, password, first_name, last_name, role) VALUES('"+username+"', '"+hashed_password+"', '"+firstName+"', '"+lastName+"', '"+role+"')"
+    c.execute(query)
+    c.commit()
+    
+    #check if username was inserted into database
+    query2 = "SELECT * FROM ids WHERE username='"+username+"'"
+    c.execute(query2)
+    results = c.fetchone()
+ 
+    conn.close()
+    return results
